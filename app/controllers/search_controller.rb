2179f1682637fe08a430f7c002b1ea70b5dd6c72
mmbensalah <mmbensalah@gmail.com>
2018-10-17 10:33:26 -0600

    add to view

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,10 +1,10 @@
 class SearchController < ApplicationController
   def index
-    conn = Faraday.new(url: "https://developer.nrel.gov") do |faraday|
-      faraday.headers["x-api-key"] = ENV["nrel_key"]
-    end
-
-    @station_results = conn.get("/api/alt-fuel-stations/v1/nearest?location=80203")
+    conn = Faraday.new(url: "https://developer.nrel.gov")
+    api_key = "IaGiwuccWzp5LD7AlQOeYJruiqnUq0B3LZ91xYF4"
+    results = conn.get("/api/alt-fuel-stations/v1/nearest?location=80203&api_key=#{api_key}&format=json")
+    @station_results = JSON.parse(results.body)
     binding.pry
   end
 end
+# Faraday.get("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest?location=80203&api_key=IaGiwuccWzp5LD7AlQOeYJruiqnUq0B3LZ91xYF4&format=json")
