35a92982731ada61b1b622e2cac8e0b5d2ea8ace
mmbensalah <mmbensalah@gmail.com>
2018-10-17 10:40:35 -0600

    hardcode in ENV api key to uri

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,12 +1,8 @@
 class SearchController < ApplicationController
   def index
-    conn = Faraday.new(url: "https://developer.nrel.gov") do |faraday|
-      faraday.headers["x-api-key"] = ENV["nrel_key"]
-    end
-    api_key = "IaGiwuccWzp5LD7AlQOeYJruiqnUq0B3LZ91xYF4"
-    results = conn.get("/api/alt-fuel-stations/v1/nearest?location=80203&format=json")
+    conn = Faraday.new(url: "https://developer.nrel.gov")
+    results = conn.get("/api/alt-fuel-stations/v1/nearest?location=80203&api_key=#{ENV["nrel_key"]}&format=json")
     @station_results = JSON.parse(results.body)
     binding.pry
   end
 end
-# Faraday.get("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest?location=80203&api_key=IaGiwuccWzp5LD7AlQOeYJruiqnUq0B3LZ91xYF4&format=json")
