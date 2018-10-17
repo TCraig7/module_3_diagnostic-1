0c64bc1bd96db0ff460b4303d61fbae3e00cb28a
mmbensalah <mmbensalah@gmail.com>
2018-10-17 11:01:14 -0600

    realized results are already sorted by distance - added limit to index.html.erb

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,6 +3,5 @@ class SearchController < ApplicationController
     conn = Faraday.new(url: "https://developer.nrel.gov")
     results = conn.get("/api/alt-fuel-stations/v1/nearest?location=80203&api_key=#{ENV["nrel_key"]}&format=json&radius=6.0")
     @station_results = JSON.parse(results.body)
-    binding.pry
   end
 end
