29518eb23be8c87e4a963fa78270b54d9402f3d9
mmbensalah <mmbensalah@gmail.com>
2018-10-17 11:06:23 -0600

    add in PORO StationFetch

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,9 @@
 class SearchController < ApplicationController
   def index
-    conn = Faraday.new(url: "https://developer.nrel.gov")
-    results = conn.get("/api/alt-fuel-stations/v1/nearest?location=80203&api_key=#{ENV["nrel_key"]}&format=json&radius=6.0")
-    @station_results = JSON.parse(results.body)
+    fetch = StationFetch.new
+    @station_results = fetch.location_results
+    # conn = Faraday.new(url: "https://developer.nrel.gov")
+    # results = conn.get("/api/alt-fuel-stations/v1/nearest?location=80203&api_key=#{ENV["nrel_key"]}&format=json&radius=6.0")
+    # @station_results = JSON.parse(results.body)
   end
 end
--- app/models/station_fetch.rb
+++ app/models/station_fetch.rb
@@ -1,11 +1,10 @@
-class StationFetch 
-  def initialize(location)
-    @location = location
+class StationFetch
+  def initialize
   end
 
-  def conn
-    Faraday.new(url: "https://developer.nrel.gov") do |faraday|
-      faraday.headers["X-API-KEY"] = ENV["nrel_key"]
-    end
+  def location_results
+    conn = Faraday.new(url: "https://developer.nrel.gov")
+    results = conn.get("/api/alt-fuel-stations/v1/nearest?location=80203&api_key=#{ENV["nrel_key"]}&format=json&radius=6.0")
+    station_results = JSON.parse(results.body)
   end
 end
