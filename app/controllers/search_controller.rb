8b99bd126b0fd11bed5996e5ed2deb5233d7419b
mmbensalah <mmbensalah@gmail.com>
2018-10-17 10:38:59 -0600

    trying to remove hardcoded api key - not working

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,10 @@
 class SearchController < ApplicationController
   def index
-    conn = Faraday.new(url: "https://developer.nrel.gov")
+    conn = Faraday.new(url: "https://developer.nrel.gov") do |faraday|
+      faraday.headers["x-api-key"] = ENV["nrel_key"]
+    end
     api_key = "IaGiwuccWzp5LD7AlQOeYJruiqnUq0B3LZ91xYF4"
-    results = conn.get("/api/alt-fuel-stations/v1/nearest?location=80203&api_key=#{api_key}&format=json")
+    results = conn.get("/api/alt-fuel-stations/v1/nearest?location=80203&format=json")
     @station_results = JSON.parse(results.body)
     binding.pry
   end
