74b8bbca8bfe65489d6cddbe58bc3614757f8040
mmbensalah <mmbensalah@gmail.com>
2018-10-17 10:15:20 -0600

    still working on fetching api

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,10 @@
 class SearchController < ApplicationController
   def index
-    api = StationFetch.new(params[:location])
-    results = api.conn
-    @station_results = results.get("api/alt-fuel-stations/v1/nearest?location=80203")
+    conn = Faraday.new(url: "https://developer.nrel.gov") do |faraday|
+      faraday.headers["x-api-key"] = ENV["nrel_key"]
+    end
+
+    @station_results = conn.get("/api/alt-fuel-stations/v1/nearest?location=80203")
     binding.pry
   end
 end
